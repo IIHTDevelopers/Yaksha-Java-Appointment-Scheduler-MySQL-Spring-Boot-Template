e20e0dfb4b964a2a321abf5ba49a51decc8ebbb7
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-18 19:55:07 +0530

    Update DoctorService.java

--- src/main/java/com/appointment/service/DoctorService.java
+++ src/main/java/com/appointment/service/DoctorService.java
@@ -2,7 +2,12 @@ package com.appointment.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.appointment.dto.DoctorDTO;
+import com.appointment.entity.Doctor;
+import com.appointment.entity.Schedule;
 
 import javassist.NotFoundException;
 
@@ -19,4 +24,10 @@ public interface DoctorService {
 	boolean deleteDoctor(Long id) throws NotFoundException;
 
 	List<DoctorDTO> findDoctorsBySpecialty(String specialty);
+
+	List<Schedule> findSchedulesByDoctorNameAndSpecialty(String doctorName, String specialty);
+
+	List<Schedule> findSchedulesByDoctorId(Long doctorId);
+
+	Page<DoctorDTO> findAllByOrderBySpecialtyAsc(Pageable pageable);
 }
