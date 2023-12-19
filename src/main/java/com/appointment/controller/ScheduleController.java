9b21fd46f5347cfd260f682c4415a7b6a913d64b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-19 19:57:21 +0530

    Update ScheduleController.java

--- src/main/java/com/appointment/controller/ScheduleController.java
+++ src/main/java/com/appointment/controller/ScheduleController.java
@@ -3,12 +3,10 @@ package com.appointment.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.appointment.service.ScheduleService;
-
 @RestController
 @RequestMapping("/api/schedules")
 public class ScheduleController {
 
-	private final ScheduleService scheduleService = null;
+	// write your logic here
 
 }
