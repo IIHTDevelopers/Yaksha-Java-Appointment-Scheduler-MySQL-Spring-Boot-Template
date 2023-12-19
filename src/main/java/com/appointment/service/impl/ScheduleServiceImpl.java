fec929f7842b6a72278971619a61416a8c103fd3
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-19 20:09:14 +0530

    Update ScheduleServiceImpl.java

--- src/main/java/com/appointment/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/appointment/service/impl/ScheduleServiceImpl.java
@@ -1,48 +1,50 @@
 package com.appointment.service.impl;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.appointment.dto.ScheduleDTO;
+import com.appointment.exception.NotFoundException;
 import com.appointment.service.ScheduleService;
-
-import javassist.NotFoundException;
 
 @Service
 public class ScheduleServiceImpl implements ScheduleService {
 
 	@Override
 	public ScheduleDTO createSchedule(ScheduleDTO scheduleDTO) {
+		// write your logic here
 		return null;
 	}
 
 	@Override
 	public ScheduleDTO updateSchedule(Long id, ScheduleDTO scheduleDTO) throws NotFoundException {
+		// write your logic here
 		return null;
 	}
 
 	@Override
 	public List<ScheduleDTO> getAllSchedules() {
+		// write your logic here
 		return null;
 	}
 
 	@Override
 	public ScheduleDTO getScheduleById(Long id) throws NotFoundException {
+		// write your logic here
 		return null;
 	}
 
 	@Override
 	public boolean deleteSchedule(Long id) throws NotFoundException {
+		// write your logic here
 		return false;
 	}
 
 	@Override
-	public List<ScheduleDTO> getDoctorAppointments(Long doctorId, String day) {
-		return null;
-	}
-
-	public List<ScheduleDTO> getDoctorScheduleByDay(Long doctorId, String day) {
+	public List<ScheduleDTO> getDoctorScheduleByDay(Long doctorId, LocalDate day) {
+		// write your logic here
 		return null;
 	}
 }
