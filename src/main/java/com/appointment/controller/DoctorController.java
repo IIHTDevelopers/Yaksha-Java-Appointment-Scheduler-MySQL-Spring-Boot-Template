bf6a98151ba8ba4d6911d62c5c7bd9da05907282
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-19 19:56:52 +0530

    Update DoctorController.java

--- src/main/java/com/appointment/controller/DoctorController.java
+++ src/main/java/com/appointment/controller/DoctorController.java
@@ -3,12 +3,10 @@ package com.appointment.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.appointment.service.DoctorService;
-
 @RestController
 @RequestMapping("/api/doctors")
 public class DoctorController {
 
-	private final DoctorService doctorService = null;
+	// write your logic here
 
 }
