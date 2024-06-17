0ee4b66641ad42997bb63de0a77853ff6c238a17
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-06-17 20:43:36 +0530

    Add files via upload

---
+++ src/main/java/com/appointment/service/impl/DoctorServiceImpl.java
@@ -0,0 +1,49 @@
+package com.appointment.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.appointment.dto.DoctorDTO;
+import com.appointment.exception.NotFoundException;
+import com.appointment.service.DoctorService;
+
+@Service
+public class DoctorServiceImpl implements DoctorService {
+
+	@Override
+	public DoctorDTO createDoctor(DoctorDTO doctorDTO) {
+		// write your logic here
+		return null;
+	}
+
+	@Override
+	public DoctorDTO updateDoctor(Long id, DoctorDTO doctorDTO) throws NotFoundException {
+		// write your logic here
+		return null;
+	}
+
+	@Override
+	public List<DoctorDTO> getAllDoctors() {
+		// write your logic here
+		return null;
+	}
+
+	@Override
+	public DoctorDTO getDoctorById(Long id) throws NotFoundException {
+		// write your logic here
+		return null;
+	}
+
+	@Override
+	public boolean deleteDoctor(Long id) throws NotFoundException {
+		// write your logic here
+		return false;
+	}
+
+	@Override
+	public List<DoctorDTO> findDoctorsBySpecialty(String specialty) {
+		// write your logic here
+		return null;
+	}
+}
